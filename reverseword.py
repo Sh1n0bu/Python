e765c6eecd041d510d6dd4a876391f598b004885
Sh1n0bu <hisokageney@gmail.com>
2018-05-27 14:41:17 -0300

    Bug fixes and translated for english

--- reverseword.py
+++ reverseword.py
@@ -5,11 +5,11 @@ print(a[-11],a[-10],a[-9],a[-8],a[-7],a[-6],a[-5],a[-4],a[-3],a[-2],a[-1])
 #and from right to the left .Lef to the right use values from 0 to the 
 #final caracter and from right to the left use values from -1 to the 
 #(-) value of first character
-phrase = input("Type a phrase: ")
-i = int(-len(phrase))
+word = input("Type a word: ")
+i = int(-len(word))
 f = -1
-print("A frase ao contrario e :", end="")
+print("The reverse word is  :", end="")
 while f >= i:
-	print(phrase[f] , end ="")
+	print(word[f] , end ="")
 	f = f-1
 print()
