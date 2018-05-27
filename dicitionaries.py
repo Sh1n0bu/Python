8c0ae96aeebefde18630a654a25a91e7564cca7b
Sh1n0bu <hisokageney@gmail.com>
2018-05-27 15:38:24 -0300

    Added two new programs about dictionaries and lists/tuples

---
+++ dicitionaries.py
@@ -0,0 +1,3 @@
+personal_info ={"Name":"Linus" , "Surname":"Torvalds",
+"Relevancy":"Creator of Linux and Git"}
+print(personal_info["Surname"]
---
+++ listsandtuples.py
@@ -0,0 +1,10 @@
+#lists and tuples 
+lost = [5,"lain",766,15 ,2.7]
+last = (9, "wired", 909, 876)
+#The diference between lists and tuples is that liss is mutable and 
+#tuples are not mutables(you can't add or remove things )
+#Other diference is , in lists we use square brackets , in tuples
+#we use parenthesis 
+lost.append("Navi")#You can't append in tuples
+print(lost)
+
