e119032faab94535e43f33fca3eb6b1da1d921e7
Sh1n0bu <hisokageney@gmail.com>
2018-05-27 12:15:33 -0300

    Added operations with strings

--- cryptography.py
+++ cryptography.py
@@ -1,4 +1,7 @@
 #I made this program to study methods and operations
 phrase = input ("Type a phrase :")
 phrase = phrase.replace("a", "z")#Will replace a for z
+new_phrase = input("Type other phrase: ")
+new_phrase += "Pamyu Pamyu"
 print(phrase)
+print(new_phrase)
