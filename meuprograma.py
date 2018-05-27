c307b1c9bb2177d9c5ce9381a51505ff29210dcf
Sh1n0bu <hisokageney@gmail.com>
2018-05-27 11:25:31 -0300

    Modified in the sequential lesson

--- meuprograma.py
+++ meuprograma.py
@@ -1,3 +1,8 @@
-_greetings = input("Digite seu nome")
-print(_greetings)
+age = input("Enter your age: ") 
 
+# The input function catchs a string in every case and we have
+# to convert the vlue typed by the user
+						  
+#new_age = age + 50  /// We have to convert the age of string for int
+new_age = int(age) + 50
+print(new_age)
