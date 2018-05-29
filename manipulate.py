743257fa3aa0e5e160b03717681cdd36e25fdb15
Sh1n0bu <hisokageney@gmail.com>
2018-05-29 20:16:04 -0300

    Modified the manipulate.py.Now manipulate.py show the lenght of the fruit name in list

--- manipulate.py
+++ manipulate.py
@@ -1,4 +1,11 @@
 file = open("fruits.txt", 'r')
-content = file.read()
+content = file.readlines()
+content = [i.rstrip() for i in content]
 file.close
-print(content)
+a = 0
+for k in content:
+	print(len(content[a]))
+	a+=1
+#Other form to do this 
+for s in content:
+	print(len(s.strip()))#In this case , the line 3,5,8 is unnecessary
