f013433a5a4cdae01ae972d8774b0e5a91d548ab
Sh1n0bu <hisokageney@gmail.com>
2018-05-29 18:24:42 -0300

    Modified the celsiustofahrenheit using the lists , and added a litle program using the loop for

--- celsiustofahrenheit.py
+++ celsiustofahrenheit.py
@@ -2,12 +2,22 @@
 def celsius_to_fahrenheit(celsius):
 	fahrenheit = (9*(celsius))/5 +32
 	return fahrenheit
-celsius = float(input("Type the temperature in celsius: "))
 
-if celsius < -273.15 :
-	print("The minimun temperature is the absolute zero , thats equal to -273.15")
-	
-else:
-	print(celsius_to_fahrenheit(celsius))
+#celsius = float(input("Type the temperature in celsius: "))
 
-	
+#if celsius < -273.15 :
+#	print("The minimun temperature is the absolute zero , thats equal to -273.15")
+
+#else:
+#	print(celsius_to_fahrenheit(celsius))
+temperature=[10, -20, -289, 100]
+a = 0
+
+for i in temperature:
+	if temperature[a] < -273.15:
+		print("The minimun temperature is the absolute zero , thats equal to -273.15")
+	else :
+		print(celsius_to_fahrenheit(temperature[a]))
+	a = a+1
+		
+
