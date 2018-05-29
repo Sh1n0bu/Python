eeacb7c37181e174ced38ffb45102277703ecbad
Sh1n0bu <hisokageney@gmail.com>
2018-05-29 19:41:17 -0300

    The celsiustofahrenheit was simplified and added a python program that open a text file and print the content in terminal

--- celsiustofahrenheit.py
+++ celsiustofahrenheit.py
@@ -2,22 +2,17 @@
 def celsius_to_fahrenheit(celsius):
 	fahrenheit = (9*(celsius))/5 +32
 	return fahrenheit
+temperature = [10, -20, -289, 100]
+celsius = float(input("Type the temperature in celsius: "))
 
-#celsius = float(input("Type the temperature in celsius: "))
+if celsius < -273.15 :
+	print("The minimun temperature is the absolute zero , thats equal to -273.15")
 
-#if celsius < -273.15 :
-#	print("The minimun temperature is the absolute zero , thats equal to -273.15")
-
-#else:
-#	print(celsius_to_fahrenheit(celsius))
+else:
+	print(celsius_to_fahrenheit(celsius))
 temperature=[10, -20, -289, 100]
-a = 0
-
 for i in temperature:
-	if temperature[a] < -273.15:
-		print("The minimun temperature is the absolute zero , thats equal to -273.15")
-	else :
-		print(celsius_to_fahrenheit(temperature[a]))
-	a = a+1
-		
+	print(celsius_to_fahrenheit(i))
+
+
 
